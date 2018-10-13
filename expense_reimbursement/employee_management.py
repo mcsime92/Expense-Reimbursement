ad34b35f709d296afb694ab5bc8a349ba5f2de69
Maxime <gmdevlab@gmail.com>
2018-10-13 18:20:55 +0200

    Added NewEntry object, functions for name/id and summary function

--- expense_reimbursement/employee_management.py
+++ expense_reimbursement/employee_management.py
@@ -2,28 +2,50 @@ from random import randint
 
 employee_list = []
 
-def gen_employee_name():
-    input_first_name = input('> Employee first name ')
-    input_last_name = input('> Employee last name ')
-    return input_first_name, input_last_name
+
+class NewEntry(object):
+    def gen_employee_name(self):
+        input_first_name = input('> Employee first name ')
+        input_last_name = input('> Employee last name ')
+        return input_first_name, input_last_name
+
+    def gen_employee_id(self):
+        employee_id = randint(1, 1000)
+        return employee_id
+
+    def summary_entry(self):
+
+        first_name, last_name = NewEntry().gen_employee_name()  # unpacking
+        employee_id = NewEntry().gen_employee_id()
+
+        print(first_name, last_name, '- Employee ID:', employee_id)
+
+        new_employee_entry = (employee_id, first_name, last_name)
+        employee_list.append(new_employee_entry)
+
+    # TODO: Increment employee ID
 
 
-def gen_employee_id():
-    employee_id = randint(1, 1000)
-    return employee_id
-
-#TODO: Increment employee ID
-
-first_name, last_name = gen_employee_name() # unpacking
-employee_id = gen_employee_id()
-
-print(first_name, last_name, '- Employee ID:', employee_id)
-
-new_entry = (employee_id, first_name, last_name)
-
-employee_list.append(new_entry)
+new_entry = NewEntry()
+new_entry.summary_entry()  # This trigger the whole NewEntry class.
 
 
-#TODO: Delete employee
-#TODO: Update employee
+def additional_entry():
+    if answer == 'Y':
+        new_entry.summary_entry()
 
+    elif answer == 'N':
+        print('Done with employee management.')
+    else:
+        print('Please answer by Y or N')
+
+
+# TODO: Delete employee
+# TODO: Update employee
+
+answer = input('Would you like to add more entries ? Y or N ')
+additional_entry()
+
+print(employee_list)
+
+print('bye')
