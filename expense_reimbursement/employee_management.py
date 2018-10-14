2ce4d6f18750aae2f42d358f6a1588b06258dd7b
Maxime <gmdevlab@gmail.com>
2018-10-14 14:07:37 +0200

    Added menu management, with show_entry function to print given line from database

--- expense_reimbursement/employee_management.py
+++ expense_reimbursement/employee_management.py
@@ -1,9 +1,42 @@
 from random import randint
 import pandas as pd
+import csv
 from pathlib import Path
 
 employee_list = []
 employee_db = 'employee_list.csv'
+
+
+class DataManagement(object):
+    def data_to_file(self):
+        my_file = Path(employee_db)
+        if my_file.is_file():
+            with open(employee_db, 'a') as ed:
+                for item in employee_list:
+                    ed.write((",".join(map(str, item))))
+                    ed.write("\n")
+        else:
+            df = pd.DataFrame(employee_list, columns=["EmployeeID", "FirstName", "LastName"])
+            df.to_csv(employee_db, sep=',', index=False)
+
+    def show_entry(self):
+        my_file = Path(employee_db)
+        if my_file.is_file():
+            record = input('Enter employee ID, first or last name ')
+            csv_file = csv.reader(open(employee_db, 'rt', encoding = 'utf-8'), delimiter=',')
+            for row in csv_file:
+                if record == row[0] or record == row[1] or record == row[2]:
+                    print(row)
+
+
+        else:
+            print('There is no such file')
+
+    def delete_entry(self):
+        pass
+
+    def update_entry(self):
+        pass
 
 
 class NewEntry(object):
@@ -49,23 +82,39 @@ class ManageEntry(object):
         pass
 
 
+manage_entry = ManageEntry()
+new_entry = NewEntry()
+data_management = DataManagement()
+
+
 # TODO 1: Increment employee ID or add a check to prevent duplicates
 # TODO 3: Delete employee
 # TODO 4: Update employee
 # TODO 5: Try catch for PermissionError: [Errno 13] Permission denied: 'employee_list.csv'
+# TODO : Contextual menu: 1. New entry, 2. Manage entries, 3. Quit
 
-new_entry = NewEntry()
-new_entry.summary_entry()  # This trigger the whole NewEntry class.
+
+def functionality_choice():
+    functionality = input('What would you lke to do? Type 1, 2 or 3:\n'
+                          '1. New entry\n'
+                          '2. Manage entries\n'
+                          '3. Quit\n')
+
+    if functionality == "1":
+        NewEntry().summary_entry()
+    elif functionality == "2":
+        DataManagement().show_entry()
+    elif functionality == "3":
+        print('Exit')
+        exit()
+    else:
+        print('Please answer 1, 2 or 3')
+        functionality_choice()
+
+
+print('>>> Employee Management Interface <<<\n')
+functionality_choice()
 
 # print(employee_list) // for testing
 
 # Checks if a file exists. If yes, uses it. If no, creates it with data.
-my_file = Path(employee_db)
-if my_file.is_file():
-    with open(employee_db, 'a') as ed:
-        for item in employee_list:
-            ed.write((",".join(map(str, item))))
-            ed.write("\n")
-else:
-    df = pd.DataFrame(employee_list, columns=["EmployeeID", "FirstName", "LastName"])
-    df.to_csv(employee_db, sep=',', index=False)
